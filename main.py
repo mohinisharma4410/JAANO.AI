ad254fe154cc016d5975e07e2ac586e6ad10c1b2
mohinisharma4410 <111170118+mohinisharma4410@users.noreply.github.com>
2025-07-22 17:45:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -323,5 +323,5 @@ def generate_video_cloud():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
